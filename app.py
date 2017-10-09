85f7de8e636f45a56e88dd832d1503f339eadee9
Sunny Chakraborty <sunnyc7@gmail.com>
2017-10-09 04:23:41 -0400

    Fixed tests. Tests passing now

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 import signal
 
-import trader as trad
+from trader import generator
 
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 if __name__ == "__main__":
-    trad.generator.display_menu()
+    generator.display_menu()
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,21 +1,17 @@
 import unittest
-import trader as trad
+from trader import generator
+
 #This needs to be completely overhauled at a later period
 
-class TestGetQuote(unittest.TestCase):
-
-    def test_type_get_quote():
-        word = trad.generator.get_quote("AAPL")
-        self.assertEqual(type(word), float64)
-        self.assertFalse(type(word), str)
+def test_type_get_quote():
+    word = generator.get_quote("AAPL")        
+    assert 'AAPL' in word
+    #assert 'float64' in word
+    assert type(word) != str
         
     '''
-    sdef test_get_quote():
+    def test_get_quote():
         l = ('AAPL')    
         word = trad.generator.get_quote("AAPL")
         assert l in word
     '''
-
-if __name__ == '__main__':
-    unittest.main()
-
